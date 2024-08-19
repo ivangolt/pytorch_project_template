1e41d35fb028129a345ee0be540735062728d5ea
ivan golt <102701882+ivangolt@users.noreply.github.com>
2024-08-19 19:57:42 +0300

    feat: realize argparse in cli

--- scrpits/inference.py
+++ scrpits/inference.py
@@ -1,11 +1,29 @@
+import argparse
+
 import numpy as np
 import onnxruntime as ort
 
 from utils.preprocess_input_image import preprocess_image
 
-onnx_model_path = "./outputs/models/LeNet5.onnx"
+# onnx_model_path = "./outputs/models/LeNet5.onnx"
 
-image_path = "C:/VSCode_projects/pytorch_project_template/data/mnist/test/7_000638.png"
+# image_path = "C:/VSCode_projects/pytorch_project_template/data/mnist/test/7_000638.png"
+
+parser = argparse.ArgumentParser(description="model inference")
+parser.add_argument("--model", type=str, help="Model name (model1, model2)")
+parser.add_argument(
+    "--path_of_image", type=str, default=None, help="Path for image to inference"
+)
+args = parser.parse_args()
+
+if args.model.lower() == "lenet":
+    model = "./outputs/models/LeNet5.onnx"
+# elif args.model.lower() == "mnist_model":
+#     model = MnistModel()
+# elif args.model.lower() == "cnn":
+#     model = CNN()
+else:
+    raise ValueError(f"Unsupported model: {args.model}")
 
 
 def onnx_inference(image_path: str, model_path: str):
@@ -34,4 +52,4 @@ def onnx_inference(image_path: str, model_path: str):
     return f"Predicted label: {output_tensor.argmax()}"
 
 
-print(onnx_inference(image_path=image_path, model_path=onnx_model_path))
+print(onnx_inference(image_path=args.path_of_image, model_path=args.model))
